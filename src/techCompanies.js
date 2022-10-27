a6ba671a995a0bb94512322e5ef6d34e62baa20d
Sproff <sproff.oluwaseun@gmail.com>
2022-10-27 22:43:50 +0100

    feat: 🎸 Adds content from techCompanies.json to techCompanies.j

--- src/techCompanies.js
+++ src/techCompanies.js
@@ -490,6 +490,13 @@ export const techCompanies = [
         industry: "FinTech",
         website: "https://www.lemonade.finance",
       },
+      {
+        companyName: "LakexIO Technologies",
+        companyTwitterHandle: "lakex_io",
+        foundersTwitterHandle: ["o_o_lekan"],
+        industry: "Tech Agency",
+        website: "",
+      },
     ],
   },
   {
