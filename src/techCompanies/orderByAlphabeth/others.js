3f8c89224815868a4685ecd7076831581cd8ae44
Ezinne Anne Emilia <onwukaezinneanne@gmail.com>
2024-10-08 10:54:08 +0100

    Fixed issue: Remove Abegapp and 54gene #261

--- src/techCompanies/orderByAlphabeth/others.js
+++ src/techCompanies/orderByAlphabeth/others.js
@@ -1,12 +1,5 @@
 export default {
   startsWith: "*",
   companies: [
-    {
-      companyName: "54gene",
-      companyTwitterHandle: "weare54gene",
-      foundersTwitterHandle: ["AbasiEneobong"],
-      industry: "Health Tech",
-      website: "https://54gene.com",
-    },
   ],
 };
